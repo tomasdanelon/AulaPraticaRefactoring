6ae33d459d9387568185a22b58449c81d33fa318
tomasdanelon <tomasdanelon@gmail.com>
2022-12-06 00:37:47 -0300

    Commit 17

--- htmlstatement.java
+++ htmlstatement.java
@@ -1,21 +1,30 @@
 import java.util.Enumeration;
 public class HtmlStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
-      }
-      //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
-      return result;
-   }
+    private String setHeader(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() +
+        "</EM></H1><P>\n";
+    }
+    private String setBody(Rental aRental) {
+        return aRental.getMovie().getTitle()+ ": " +
+            String.valueOf(aRental.getCharge()) + "<BR>\n";
+    }
+    private String setFooter(Customer aCustomer) {
+        return "<P>You owe <EM>" +
+        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" + 
+        "On this rental you earned <EM>" + 
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        "</EM> frequent renter points<P>";
+    }
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = setHeader(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += setBody(each);
+        }
+        //add footer lines
+        result += setFooter(aCustomer);
+        return result;
+    }
 }
--- textstatement.java
+++ textstatement.java
@@ -1,21 +1,29 @@
 import java.util.Enumeration;
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
-      }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
-      return result;
-   }
+    private String setHeader(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() + "\n";
+    }
+    private String setBody(Rental aRental) {
+        return "\t" + aRental.getMovie().getTitle()+ "\t" +
+            String.valueOf(aRental.getCharge()) + "\n";
+    }
+    private String setFooter(Customer aCustomer) {
+        return "Amount owed is " +
+        String.valueOf(aCustomer.getTotalCharge()) + "\n" + 
+        "You earned " +
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        " frequent renter points";
+    }
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = setHeader(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += setBody(each);
+        }
+        //add footer lines
+        result += setFooter(aCustomer);
+        return result;
+    }
 }
