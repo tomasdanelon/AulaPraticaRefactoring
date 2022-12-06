054cef74139e9aa83b4681e7b6edf6df98f1c3f9
tomasdanelon <tomasdanelon@gmail.com>
2022-12-06 00:25:46 -0300

    Commit 11

--- movie.java
+++ movie.java
@@ -5,16 +5,16 @@ public class Movie {
     public static final int  NEW_RELEASE = 1;
   
     private String _title;
-    private int _priceCode;
+    private Price _price;
   
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
   
     public double getCharge(int daysRented) {
         double thisAmount = 0;
-        switch (_priceCode) {
+        switch (_price.getPriceCode()) {
            case Movie.REGULAR:
               thisAmount += 2;
               if (daysRented > 2)
@@ -34,17 +34,29 @@ public class Movie {
     
       public int getFrequentRenterPoints(int daysRented){
         // add bonus for a two day new release rental
-        if ((_priceCode  == Movie.NEW_RELEASE) &&
+        if ((_price.getPriceCode()  == Movie.NEW_RELEASE) &&
                 daysRented > 1) return 2;
         return 1;
       }
     
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
   
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+               _price = new RegularPrice();
+               break;
+            case CHILDRENS:
+               _price = new ChildrensPrice();
+               break;
+            case NEW_RELEASE:
+               _price = new NewReleasePrice();
+               break;
+            default:
+               throw new IllegalArgumentException("Incorrect Price Code");
+         }
     }
   
     public String getTitle (){
