f6dfc17feed4cd6e7079e09369734a17da2436b4
tomasdanelon <tomasdanelon@gmail.com>
2022-12-06 00:31:21 -0300

    Commit 15

--- newreleaseprice.java
+++ newreleaseprice.java
@@ -5,4 +5,7 @@ public class NewReleasePrice extends Price {
       public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
  }
