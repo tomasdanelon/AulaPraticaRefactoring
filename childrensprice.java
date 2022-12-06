07f7893959154b1c789bbada0115702f219f4a6c
tomasdanelon <tomasdanelon@gmail.com>
2022-12-06 00:29:03 -0300

    Commit 13

--- childrensprice.java
+++ childrensprice.java
@@ -1,4 +1,10 @@
 public class ChildrensPrice extends Price {
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+           result += (daysRented - 3) * 1.5;
+        return result;
+     }
     public int getPriceCode() {
         return Movie.CHILDRENS;
     }
--- newreleaseprice.java
+++ newreleaseprice.java
@@ -1,5 +1,8 @@
 public class NewReleasePrice extends Price {
-    public int getPriceCode() {
+    public double getCharge(int daysRented){
+        return daysRented * 3;
+      } 
+      public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
  }
