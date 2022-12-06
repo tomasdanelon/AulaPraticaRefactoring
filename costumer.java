5e57699bafe3ea82f044755089402d723347396f
tomasdanelon <tomasdanelon@gmail.com>
2022-12-06 00:18:27 -0300

    Commit 8

--- costumer.java
+++ costumer.java
@@ -17,23 +17,23 @@ public class Customer {
       return _name;
    }
   
-   public String statement() {
+   public String htmlStatement() {
       Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
-          Rental each = (Rental) rentals.nextElement();
+         Rental each = (Rental) rentals.nextElement();
+         // show figures for each rental
+         result += each.getMovie().getTitle()+ ": " +
+                  String.valueOf(each.getCharge()) + "<BR>\n";
+      }
 
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
-        }
-
-        // add footer lines
-        result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                        " frequent renter points";
-        return result;
-    }
+      // add footer lines
+      result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+      return result;
+   }
 
     private double getTotalCharge() {
         double result = 0;
